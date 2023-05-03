15510f56d49b1558663137aae3acc29cc559ece1
karam thebian <karam.thebian@montymobile.com>
2023-05-03 17:16:36 +0300

    how to user token

--- swagger_server/services/authorization_services/check_access_token.py
+++ swagger_server/services/authorization_services/check_access_token.py
@@ -2,12 +2,18 @@ from helpers.token_helpers import decode_token
 import connexion
 from connexion.exceptions import ProblemException
 def check_access_token(token, required_scopes):
+    # the function below attemps to decode the token. if the token is valid and has not expired, it will raise an exception telling the user the token is invalid
+    #else it is going to return a dictionary with all the details that we stored in the token, such as username and role.
     payload = decode_token(token)
 
+
+    #we are defining allowed apis that a user can call first as empty, then based on his role, we fill up his allowed APIS
+    allowed_apis =[]
+    # Fetching the users role from his token
     user_role = payload.get("role")
 
     if user_role == "admin":
-        pass
+        allowed_apis =["add_item","edit_item","get_item","get_user","add_user"]
     elif user_role == "cashier":
         allowed_apis = ["add_item","edit_item","get_item","get_user"]
 
@@ -16,7 +22,7 @@ def check_access_token(token, required_scopes):
 
     api_endpoint = connexion.request.endpoint.split("controller_")[1]
     if api_endpoint in allowed_apis:
-        #THIS IS FOR LATER ON FOR CODE TO KNOW WHO IS USING IT FROM TOKEN.
+        #THIS IS FOR LATER ON FOR CODE TO KNOW WHO IS USING IT FROM TOKEN. We are storing the token payload that includes the username and role for later use, check add item
         return {'sub': payload.get("id"),'test_key':payload}
     else:
         raise ProblemException(status=401, title="Unauthorized", detail="You are not allowed to use this API.")
--- swagger_server/services/item_services/add_item.py
+++ swagger_server/services/item_services/add_item.py
@@ -1,9 +1,23 @@
 from swagger_server.models.add_item_request import AddItemRequest
 from swagger_server.models.inline_response2002 import InlineResponse2002
 import pymongo
+import connexion
+
 
 def add_item(item_request=AddItemRequest):
+    # in order to know what user is currently using the application, we need to know context.
+    # like english language: IN WHAT CONTEXT DO YOU MEAN!
+    # to get what we stored in the authorization page, we check something called context
+    token_payload = connexion.context['token_info']['test_key']
 
+    # it will be like this, in the form of the dictionary
+    # token_payload = {'iat': 1683122192, 'exp': 1683125792, 'id': '6417204935cf606b7c6b5104', 'username': 'jana.kasmani',
+    #                  'role': 'admin'}
+
+    #so lets say we want to see the username of the current user, we say token_payload.get("username")
+    username = token_payload.get("username")
+    print("username", username)
+    # so now, u can do whatever u want with the data, u can get the user from mongo by filtering his username, lets say he has a balance u can get it too and update it if u want to buy, and etc... or if u want to store who added the item in mongo, u add the username to the dictionary of the item and store it
     myclient = pymongo.MongoClient("mongodb+srv://janakasamani:ZDnkZaXIPslCBvRk@cluster0.txlg72g.mongodb.net/test")
     database = myclient["super_market"]
     collection = database["items"]
